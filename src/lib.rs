c63c7e05651b8bb5dee418a6e3d4db6f3f787a04
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2024-02-28 07:23:58 +0100

    Adding console_log and console_error macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,57 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! console_log
+//!
+//! This module contains macros for logging to the console
+//! using the `std::println!` or `std::eprintln!` macros, but also returns the
+//! formatted string.
+
+/// Logs to the console using `std::println!` macro
+/// and returns the formatted string.
+///
+/// # Examples
+///
+/// ```rust
+/// use console_log::console_log;
+///
+/// let s1 = "Hello";
+/// let s2 = String::from("world!");
+/// let n1 = 123;
+/// let output = console_log!("{}, {} {}", s1, s2, n1);
+/// assert_eq!(output, format!("{}, {} {}", s1, s2, n1));
+/// ```
+/// This will log "Hello, world!" to the console.
+#[macro_export]
+macro_rules! console_log {
+    ($($arg:tt)*) => {
+        {
+            std::println!($($arg)*);
+            std::format!($($arg)*)
+        }
+    }
+}
+
+/// Logs to the console using `std::eprintln!` macro
+/// and returns the formatted string.
+///
+/// # Examples
+///
+/// ```rust
+/// use console_log::console_log;
+///
+/// let s1 = "Hello";
+/// let s2 = String::from("world!");
+/// let n1 = 123;
+/// let output = console_log!("{}, {} {}", s1, s2, n1);
+/// assert_eq!(output, format!("{}, {} {}", s1, s2, n1));
+/// ```
+/// This will log "Hello, world!" to the console.
+#[macro_export]
+macro_rules! console_error {
+    ($($arg:tt)*) => {
+        {
+            std::eprintln!($($arg)*);
+            std::format!($($arg)*)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +59,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_logs_to_log() {
+        let s1 = "Hello";
+        let s2 = String::from("world!");
+        let n1 = 123;
+        let output = console_log!("{}, {} {}", s1, s2, n1);
+        assert_eq!(output, format!("{}, {} {}", s1, s2, n1));
+        // Hello, world! 123
+    }
+
+    #[test]
+    fn it_logs_to_error() {
+        let s1 = "Hello";
+        let s2 = String::from("world!");
+        let n1 = 123;
+        let output = console_error!("{}, {} {}", s1, s2, n1);
+        assert_eq!(output, format!("{}, {} {}", s1, s2, n1));
+        // Hello, world! 123
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use console_log::*;
+
+fn main() {
+    console_log!("Hello, world!");
+    console_error!("Hello, world!");
+}
